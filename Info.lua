0af274782e7602b3ce02083780b12387f144ce27
s-g-k <seb@internetfriendsforever.com>
2021-08-22 18:41:04 +0200

    initial commit

---
+++ Info.lua
@@ -0,0 +1,10 @@
+return {
+  VERSION = { major=0, minor=0, revision=1, },
+ 
+  LrSdkVersion = 10.0,
+  LrSdkMinimumVersion = 4.0,
+ 
+  LrToolkitIdentifier = "com.aaa.lightroom-random-image",
+  LrPluginName = "AAA Random Image",
+  LrPluginInfoUrl="https://salt.aaa.pm/notes/adobe-lightroom-classic-random-plugin",
+}
