7f95b9f4b6c160d3487a6871be55dbf3d058756c
s-g-k <seb@internetfriendsforever.com>
2021-08-22 20:23:38 +0200

    Read length of catalog and send message to UI

--- develop-random-image.lua
+++ develop-random-image.lua
@@ -1,9 +1,18 @@
 local LrLogger = import 'LrLogger'
 local LrDialogs = import 'LrDialogs'
+local LrApplication = import 'LrApplication'
+local LrTasks = import 'LrTasks'
 
 local logger = LrLogger('DevelopRandomImagePlugin')
 logger:enable('logfile')
 local log = logger:quickf('info')
 
-log('Test')
-LrDialogs.message('Hello from random image')
+LrTasks.startAsyncTask(function ()
+  log('Starting async plugin…')
+
+  local catalog = LrApplication.activeCatalog()
+  local photos = catalog:getAllPhotos()
+
+  LrDialogs.message(string.format('%d total images', #photos))
+
+end)
