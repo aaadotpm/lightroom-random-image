04fbd535b35b876eb2528a05c11ad52391537209
s-g-k <seb@internetfriendsforever.com>
2021-08-22 20:10:51 +0200

    enable basic logging to file

--- develop-random-image.lua
+++ develop-random-image.lua
@@ -1,3 +1,9 @@
+local LrLogger = import 'LrLogger'
 local LrDialogs = import 'LrDialogs'
 
+local logger = LrLogger('DevelopRandomImagePlugin')
+logger:enable('logfile')
+local log = logger:quickf('info')
+
+log('Test')
 LrDialogs.message('Hello from random image')
